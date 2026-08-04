52ffb792fdfa567d20dfd29b4568494cf2d8585f
Basilraju-10 <basilraju010@gmail.com>
2026-08-04 12:11:08 +0530

    Add new components for the "Who We Are" page and associated assets
    - Implemented Footer component with social media links and sitemap.
    - Created LeadershipTeam component to showcase key personnel.
    - Developed OurStory component detailing the company's history and mission.
    - Added OurValues component to highlight available spare parts.
    - Introduced ServicesSection component outlining service offerings.
    - Updated the main WhoWeArePage to include new components.
    - Added various image assets for components including logos and team photos.

--- app/whoweare/page.tsx
+++ app/whoweare/page.tsx
@@ -1,6 +1,11 @@
 import Layout from "@/components/Layout";
 import HeroBanner from "./components/HeroBanner";
 import AboutSection from "./components/AboutSection";
+import OurValues from "./components/OurValues";
+import OurStory from "./components/OurStory";
+import LeadershipTeam from "./components/LeadershipTeam";
+import ServicesSection from "./components/ServicesSection";
+import Footer from "./components/Footer";
 
 
 
@@ -9,6 +14,11 @@ export default function WhoWeArePage() {
     <Layout>
       <HeroBanner/>
       <AboutSection/>
+      <OurValues/>
+      <OurStory/>
+      <LeadershipTeam/>
+      <ServicesSection/>
+      <Footer/>
     </Layout>
   );
 }
